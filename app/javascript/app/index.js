bf884e30469cd3c488935fa12a3bc31d87fae96e
Bill Bryan Baduya <115074618+bcube90@users.noreply.github.com>
2023-04-21 23:25:10 +0800

    Intialized react app

---
+++ app/javascript/app/index.js
@@ -0,0 +1,10 @@
+import {createRoot} from 'react-dom/client';
+import React from 'react';
+
+const container = document.getElementById('root')
+
+createRoot(container).render(
+  <div>
+    Initializing XYZ App
+  </div>
+)
