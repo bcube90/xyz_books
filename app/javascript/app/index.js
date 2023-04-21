3f21e3c445ec3bab3ef97e3068bfa8d3876518d8
Bill Bryan Baduya <115074618+bcube90@users.noreply.github.com>
2023-04-22 02:41:28 +0800

    Installed react-bootstrap to use bootstrap without JQuery

--- app/javascript/app/index.js
+++ app/javascript/app/index.js
@@ -1,10 +1,27 @@
 import {createRoot} from 'react-dom/client';
 import React from 'react';
 
+import Alert from 'react-bootstrap/Alert';
+import Badge from 'react-bootstrap/Badge';
+
 const container = document.getElementById('root')
 
 createRoot(container).render(
   <div>
     Initializing XYZ App
+    <Alert variant="success">
+      <Alert.Heading>Hey, nice to see you</Alert.Heading>
+      <p>
+        Aww yeah, you successfully read this important alert message. This
+        example text is going to run a bit longer so that you can see how
+        spacing within an alert works with this kind of content.
+      </p>
+      <hr />
+      <p className="mb-0">
+        Whenever you need to, be sure to use margin utilities to keep things
+        nice and tidy.
+      </p>
+    </Alert>
+    Example heading <Badge bg="secondary">New</Badge>
   </div>
 )
