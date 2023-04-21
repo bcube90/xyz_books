4884e093306d476a6feed36663f5de895d860ab6
Bill Bryan Baduya <115074618+bcube90@users.noreply.github.com>
2023-04-22 04:07:13 +0800

    Organized and renamed media to not contain spaces

--- app/javascript/app/index.js
+++ app/javascript/app/index.js
@@ -3,12 +3,14 @@ import React from 'react';
 
 import Alert from 'react-bootstrap/Alert';
 import Badge from 'react-bootstrap/Badge';
+import Image from 'react-bootstrap/Image'
 
 const container = document.getElementById('root')
 
 createRoot(container).render(
   <div>
     Initializing XYZ App
+    <Image src="/media/images/banner-bg.png" roundedCircle={true} width={100} height={100}/>
     <Alert variant="success">
       <Alert.Heading>Hey, nice to see you</Alert.Heading>
       <p>
