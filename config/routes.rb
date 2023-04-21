aef3aaebc1e465c32f8fabd959825de1e7f64ce3
Bill Bryan Baduya <115074618+bcube90@users.noreply.github.com>
2023-04-21 23:46:18 +0800

    Initialized api namespace

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   root "pages#index"
 
   resources :pages, only: [:index]
+
+  namespace :api, defaults: { format: :json, subdomain: "api" } do
+    resources :books, only: [:show]
+  end
 end
