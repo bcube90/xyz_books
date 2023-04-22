c76f7e98ca5d2ae356527200b07326dce9309a6b
Bill Bryan Baduya <115074618+bcube90@users.noreply.github.com>
2023-04-22 15:50:16 +0800

    Moved header cta to section element

--- app/javascript/app/components/layouts/header/HeaderLayout.js
+++ app/javascript/app/components/layouts/header/HeaderLayout.js
@@ -1,18 +1,16 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 
-import { Col, Image } from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
 import { ReactSVG } from "react-svg";
 
 export default HeaderLayout = () => {
   return (
-    <header className="row p-0 m-0">
-      <Col xs={12} className="header-cta">
-        <div className="d-flex w-100 align-items-center justify-content-center">
-          <ReactSVG className="cta-start" src="/media/svg/26.svg" />
-          <span className="fs-6 text-white mx-2" >Save up to $500 per year on millions of book titles!</span>
-          <ReactSVG src="/media/svg/26.svg" />
-        </div>
-      </Col>
-    </header>
+    <Fragment>
+      <section className="header-cta d-flex w-100 align-items-center justify-content-center">
+        <ReactSVG className="cta-start" src="/media/svg/26.svg" />
+        <span className="fs-6 text-white mx-2" >Save up to $500 per year on millions of book titles!</span>
+        <ReactSVG src="/media/svg/26.svg" />
+      </section>
+    </Fragment>
   );
 }
